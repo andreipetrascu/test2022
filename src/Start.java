c078a179a95c6dc5071a549bc9b3ed2ea00e1c96
andreipetrascu <petrascuandrei@yahoo.com>
2022-01-26 10:11:12 +0200

    Master - comment andrei

--- src/Start.java
+++ src/Start.java
@@ -2,5 +2,6 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("First text here");
         // FIRST COMMENT ON MASTER
+        // Master - Andrei Petrascu
     }
 }
