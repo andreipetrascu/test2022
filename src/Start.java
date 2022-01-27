8106444633c69d1d938d85298a19249d7ebb9470
andreipetrascu <petrascuandrei@yahoo.com>
2022-01-27 13:10:08 +0200

    lmx1

--- src/Start.java
+++ src/Start.java
@@ -2,7 +2,6 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("First text here");
         // FIRST COMMENT ON MASTER
-        // Master - Popescu
     }
 
     public void getPrices(){
